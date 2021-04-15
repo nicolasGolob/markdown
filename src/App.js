3e304f4fe806757907aa1dd295514d89b2eed7d9
Nicolas Golob <nicolasgolob@gmail.com>
2021-04-15 11:26:02 +0200

    creation of project environment and state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import './styles/App.css'
+import {text} from './data/text'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  
+  state={text}
+
+  handleChange=(event)=>{
+    const text=event.target.value
+    this.setState({text})
+  }
+  render(){
+    const {text} = this.state
+    return(
+      <div className='container'>
+        <div className='row'>
+          <div className='col-sm-6'>
+            <textarea
+              className='form-control'
+              value={text}
+              onChange={this.handleChange}
+              rows="35"/> 
+          </div>
+          <div className='col-sm-6'>
+            <h1>Résultat</h1>
+            {text}
+          </div>
+
+        </div>
+      </div>
+
+    )
+    }
+
 }
 
 export default App;
